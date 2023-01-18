46f5df3d9c0edfbc9c107ea1cc4ccf962df10cb8
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-01-18 20:17:34 -0300

    Cria Classe abstrata Pessoa e classes PessoaCandidata e PessoaEleitora que herdam de Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,32 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+/**Classe PessoaCandidata. */
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+  
+  /**Método construtor da classe PessoaCandidata. */
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+  }
 
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+  
+  public void receberVotos() {}
+  
 }
--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,20 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+/**Classe PessoaEleitora. */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+  
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }
