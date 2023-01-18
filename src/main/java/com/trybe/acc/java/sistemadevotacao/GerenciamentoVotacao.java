9596291e4eeb4b14beadb3bfab22129a00db77e0
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-01-18 20:19:07 -0300

    Cria Classe GerenciamentoVotacao e implementa atributos e metodos necessarios

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,28 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**Classe GerenciamentoVotação. */
 public class GerenciamentoVotacao {
-
+  private ArrayList<PessoaCandidata> pessoasCanditadas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfComputado;
+  private int totalVotos;
+  
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    pessoasCanditadas.add(new PessoaCandidata(nome, numero));
+  }
+  
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
+    cpfComputado.add(cpf);
+  }
+  
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {}
+  
+  public void mostrarResultado() {}
+  
+  private double calcularPorcentagemVotos(int indicePessoaCandidata) {
+    return 0.0;
+  }
 }
